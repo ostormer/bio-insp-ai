0de72fdc230681a62e65bab1618637e169227c27
Oskar Størmer <oskar.stormer@gmail.com>
2022-02-10 09:04:08 +0100

    crossover

--- project1/sga.py
+++ project1/sga.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+from math import sin
+from typing import Tuple
+import random
 # from .LinReg import LinReg
 
 data_df = pd.read_csv("data.csv")
@@ -8,19 +11,19 @@ print(data.shape)
 n_features = data.shape[1]
 
 
-def generate_pop(n_individuals, bitstring_length) -> list:
+def generate_pop(n_individuals, bitstring_length) -> np.ndarray:
     pop = []
     for _ in range(n_individuals):
         pop.append(np.random.randint(2, size=bitstring_length))
-    return pop
+    return np.array(pop)
 
 
-def parent_selection(pop, select_frac, fitness_func, maximize=True):
+def parent_selection(pop, n_selected, fitness_func, maximize=True):
     """select parents from pop based on fitness_func
 
     Args:
-        pop (list): population, list of bitstrings
-        select_frac (float): fraction of population to select as parents (0, 1)
+        pop (np.ndarray): population, array of bitstrings
+        n_selected (int): number of population to select as parents (0, 1)
         fitness_func (function): fitness function used to evaluate fitness of each individual. fitness_func(bitstring) -> fitness
         maximize (bool, optional): True if fitness function should be maximized. Minimizes if False. Defaults to True.
     """
@@ -49,13 +52,65 @@ def parent_selection(pop, select_frac, fitness_func, maximize=True):
 
     print(sum(pop_prob))
 
-    # select select_frac parents from pop
+    # select n_selected parents from pop
+    parents = np.random.choice(pop, size=n_selected, p=pop_prob)
+
+    return parents
 
 
-def sine(x):
-    n_bits = len(x)
-    
+def flip_bit(n) -> int:
+    """turn 1 to 0 or 0 to 1
+
+    Args:
+        n (int): bit (0 or 1) to be flipped
+
+    Returns:
+        int: flipped bit
+    """
+    assert n == 0 or n == 1, "n is not 0 or 1"
+    return 1 if n == 0 else 0
+
+
+def crossover(parent_a, parent_b, mutation_chance) -> Tuple[np.ndarray, np.ndarray]:
+    child_a = np.zeros_like(parent_a)
+    child_b = np.zeros_like(parent_a)
+    # Uniform crossover, 50/50 chance
+    exchange_bits = np.random.randint(2, size=parent_a.shape)
+    for i, (a, b, exchange) in enumerate(zip(parent_a, parent_b, exchange_bits)):
+        if exchange:
+            child_a[i] = b
+            child_b[i] = a
+        else:
+            child_a[i] = a
+            child_b[i] = b
+
+        # Mutate child a
+        if random.random() < mutation_chance:
+            child_a[i] = flip_bit(child_a[i])
+        # Mutate child b
+        if random.random() < mutation_chance:
+            child_b[i] = flip_bit(child_b[i])
+
+    return child_a, child_b
+
+
+def sine(x) -> float:
+    """Converts bitstring x into number in range [0, 128] and returns sine of that number
+
+    Args:
+        x (numpy array): bitstring
+
+    Returns:
+        float: sine of bitstring
+    """
+    s = 0
+    for i, bit in enumerate(x):
+        s += bit * (2 ** (len(x) - 1 - i))
+
+    scaling_factor = 2 ** (7 - len(x))
+    return sin(s * scaling_factor)
+
 
 if __name__ == '__main__':
     pop = generate_pop(20, 4)
-    fitness_func = lambda x: 
+    print(sine(np.array([1, 0, 1, 1, 1, 1, 1])))
